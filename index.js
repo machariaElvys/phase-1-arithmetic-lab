f0990308aef4829464b26c72a0b2d4c438cfc53d
Macharia <machariaelvys@gmail.com>
2024-07-21 19:35:46 +0300

    initial commmit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1=31
+let num2=2
 
-// Write your code here
+let multiply= num1 * num2
+
+ let random = Math.floor( Math.random() * 0) + 1
+
+ let num3 = 4
+ let num4 = 18
+ let mod = num3 % num4
+
+ let max = Math.max(10,15,20)
